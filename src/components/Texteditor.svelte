52ae6364ea176f89f1bed73f1ea17cde826ebe98
Havegum <30663324+Havegum@users.noreply.github.com>
2020-01-15 13:34:17 +0100

    text editor

---
+++ src/components/Texteditor.svelte
@@ -0,0 +1,42 @@
+<script>
+import { onMount } from 'svelte';
+import pell from 'pell';
+
+let element;
+
+onMount(() => {
+  pell.init({
+    element,
+    onChange: html => console.log(html)
+  })
+});
+</script>
+
+<!-- <svelte:head>
+  <link rel="stylesheet" type="text/css" href="https://unpkg.com/pell/dist/pell.min.css">
+</svelte:head> -->
+
+<div class="texteditor" bind:this={element}></div>
+
+<style lang="scss">
+@import '../profile.scss';
+
+:global(.texteditor) {
+  @import '../../node_modules/pell/src/pell';
+}
+
+:global(.pell-actionbar) {
+  display: flex;
+}
+
+:global(.pell-content) {
+  blockquote {
+    color: $primary;
+  }
+}
+
+div {
+  width: 100%;
+  min-height: 10em;
+}
+</style>
---
+++ src/routes/create/post.svelte
@@ -0,0 +1,14 @@
+<script>
+import Texteditor from '@/components/Texteditor.svelte';
+import Button from '@/components/Button.svelte';
+</script>
+
+<svelte:head>
+  <title>Create post</title>
+</svelte:head>
+
+
+<div>
+  <Button>Upload</Button>
+  <Texteditor />
+</div>
